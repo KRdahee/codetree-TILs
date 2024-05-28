8ea7c0f7171822ff05372e9a1953daea3a3dbb0a
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-05-28 20:39:48 +0900

    Title: 세 정수형 변수 선언, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int qw = 7;
+        int er = 23;
+        int ty = 30;
+
+        System.out.println(qw + " + " + er + " = " + ty);
+        //변수 포맷은 문자열의 경우 %s를, 문자의 경우 %c를, 정수의 경우 %d, 실수의 경우 %f 등이 가능합니다.
+
+    }
+}
