9783d928290fb87dae3c1221846cddd515f8658f
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-05-29 23:02:57 +0900

    Title: 입력받아 계산, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/입력받아 계산/input-calculate.java
+++ 240529/입력받아 계산/input-calculate.java
@@ -7,3 +7,19 @@ public class Main {
         System.out.println(a);
     }
 }
+/*
+// Scanner 사용 (1)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 사용 (2)
+        Scanner sc = new Scanner(System.in);
+        // 정수 입력받기
+        int a = sc.nextInt() + 2;
+        // 출력
+        System.out.println(a);
+    }
+}
+
+*/
