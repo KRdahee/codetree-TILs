62f4a4d87244269b256e74c6c4815cf3870be8c5
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-05-29 22:31:55 +0900

    Title: 변수 값 교체하기 3, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240529/변수 값 교체하기 3/replacing-variable-values-3.java
+++ 240529/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -14,3 +14,23 @@ public class Main {
 
     }
 }
+
+/*
+temp = a;
+ a = b;
+ b = temp;
+
+public class Main {
+    public static void main (String args[]) {
+        int a = 5, b = 3;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("A is " + a + " B is " + b);
+    }
+}
+
+*/
