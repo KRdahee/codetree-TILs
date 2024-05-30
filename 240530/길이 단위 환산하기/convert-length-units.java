ed05b2920e6c5a53d780da27b90eb40b9357e9e1
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-05-30 18:42:47 +0900

    Title: 길이 단위 환산하기, Time: 150ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240530/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    //double ft = 30.48;
+
+    Scanner ft = new Scanner(System.in);
+    double n = ft.nextDouble();
+
+    System.out.printf("%.1f" , n * 30.48);
+
+    }
+}
