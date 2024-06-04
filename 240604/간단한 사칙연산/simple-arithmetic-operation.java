213292895883f875d635f31025f1aeb54a7d8276
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-06-04 15:19:42 +0900

    Title: 간단한 사칙연산, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240604/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + b);      //두 정수를 더했을 때의 결과
+    System.out.println(a - b);      //a에서 b를 뺐을 때의 결과
+    System.out.println(a%b);        //나누기 연산시 몫
+    System.out.println((double)a/b);//나머지의 경우 % 연산
+    
+    }
+}
