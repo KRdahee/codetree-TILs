b767c6d92c88ba1ee276baa7d06cbd8219effb73
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-28 16:08:48 +0900

    Title: 합과 평균, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240728/합과 평균/sum-and-avg.java
+++ 240728/합과 평균/sum-and-avg.java
@@ -7,10 +7,10 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int max = a + b;
-        double sum = max / (double) 2;
+        int max = a + b; //합
+        double sum = max / (double) 2; //평균
 
-        System.out.printf("%d %.1f", max, sum);
+        System.out.printf("%d %.1f", max, sum); //공백을 주고 합과 평균 출력하기
 
     }
 }
