774a89bc5cec5ad2cb9efdf4d42497ed970642ea
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-28 17:20:59 +0900

    Title: 큰 수에서 빼기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240728/큰 수에서 빼기/subtract-from-large-number.java
+++ 240728/큰 수에서 빼기/subtract-from-large-number.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // 두 정수 a와 b를 공백으로 구분하여 입력 받기
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        // b와 a의 차이 출력
         System.out.println(b - a);
 
         sc.close(); // Scanner 자원 반환
