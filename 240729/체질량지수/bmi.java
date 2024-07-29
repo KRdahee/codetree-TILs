105cec7103d5218dac56d5ce33c58730c8b94604
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-29 10:32:07 +0900

    Title: 체질량지수, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/체질량지수/bmi.java
+++ 240729/체질량지수/bmi.java
@@ -3,13 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double h = sc.nextInt();
-        double w = sc.nextInt();
-        double bmi = w / ((h * h) / 10000);
+        double h = sc.nextInt(); // 신장 (cm)
+        double w = sc.nextInt(); // 체중 (kg)
+
+        // 신장을 미터 단위로 변환
+        double hInMeters = h / 100.0;
+
+        // BMI 계산
+        double bmi = w / (hInMeters * hInMeters);
         
         // 소수점 없이 BMI 출력
-        int bmiRounded = (int) Math.round(bmi);
-        System.out.println(bmiRounded);
+        int bmiIntegerPart = (int) bmi;
+        System.out.println(bmiIntegerPart);
 
         // BMI 기준에 따라 출력
         if (bmi >= 25) {
