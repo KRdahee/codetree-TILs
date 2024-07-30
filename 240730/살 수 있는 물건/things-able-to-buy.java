b15c32f2a946a3e5ef4a4ef545fab15923fcab20
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-30 13:47:54 +0900

    Title: 살 수 있는 물건, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/살 수 있는 물건/things-able-to-buy.java
+++ 240730/살 수 있는 물건/things-able-to-buy.java
@@ -4,14 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int stuff = sc.nextInt();
+        int n = sc.nextInt();
 
-        if (stuff == 0) {
+        // 상품과 가격 정보
+        String[] items = {"book", "mask"};
+        int[] prices = {3000, 1000};
+
+        int maxPrice = 0;
+        String maxItem = "";
+
+        // n 이하의 가장 비싼 물건 찾기
+        for (int i = 0; i < items.length; i++) {
+            if (prices[i] <= n && prices[i] > maxPrice) {
+                maxPrice = prices[i];
+                maxItem = items[i];
+            }
+        }
+
+        if (!maxItem.isEmpty()) {
+            System.out.println(maxItem);
+        } else {
             System.out.println("no");
-        } else if (stuff >= 3000) {
-            System.out.println("book");
-        } else if (stuff <= 1500) {
-            System.out.println("mask");
         }
 
         sc.close();
