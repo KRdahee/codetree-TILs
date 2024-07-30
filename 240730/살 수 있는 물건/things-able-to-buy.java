ad5acbf7e61f51cf5823480f47f9df76fa4e9614
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-30 13:33:33 +0900

    Title: 살 수 있는 물건, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240730/살 수 있는 물건/things-able-to-buy.java
+++ 240730/살 수 있는 물건/things-able-to-buy.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    int stuff = sc.nextInt();
+        int stuff = sc.nextInt();
 
-    if(stuff >= 3000){
-        System.out.println("book");
-    }else if(stuff <= 1500 ){
-        System.out.println("mask");
-    }else if(stuff == 0){
-        System.out.println("no");
-    }
+        if (stuff >= 3000) {
+            System.out.println("book");
+        } else if (stuff <= 1500 && stuff > 0) {
+            System.out.println("mask");
+        } else if (stuff == 0) {
+            System.out.println("no");
+        } else {
+            // Handle any other cases here if needed
+            System.out.println("Unknown case");
+        }
+
+        sc.close();
     }
 }
