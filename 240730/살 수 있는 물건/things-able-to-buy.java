fdb8ee8931b7bb126bf4881dc8e783822639313e
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-30 13:27:22 +0900

    Title: 살 수 있는 물건, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240730/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int stuff = sc.nextInt();
+
+    if(stuff >= 3000){
+        System.out.println("book");
+    }else if(stuff >= 1000){
+        System.out.println("no");
+    }
+    }
+}
