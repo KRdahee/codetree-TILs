f6921a1062e0d9c909e80f8528906f718f3469d9
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-07-31 14:38:29 +0900

    Title: 살 수 있는 물건 2, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240731/살 수 있는 물건 2/things-able-to-buy-2.java
+++ 240731/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -5,11 +5,11 @@ public class Main {
        Scanner sc = new Scanner(System.in);
        int money = sc.nextInt();
 
-    if (money == 3000){
+    if (money >= 3000){
         System.out.println("book");
-    }else if(money == 1000){
+    }else if(money <= 1000){
         System.out.println("mask");
-    }else if(money == 500){
+    }else if(money <= 500){
         System.out.println("pen");
     }else{
         System.out.println("no");
