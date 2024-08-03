896f9e2ff795ccca2564ad104275e4ee72280a87
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-08-03 14:59:05 +0900

    Title: 특정 조건 두 정수 비교, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240803/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 240803/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -2,24 +2,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    int a = sc.nextInt();
-    int b = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-    if (a < b){
-        System.out.print("1");
-        System.out.print("");
-    }else if(a > b){
-        System.out.print("0");
-    }
-    if(a == b){
-        System.out.print("1");
-        System.out.print("");
-    }else if(a != b){
-        System.out.print("0");
-    }
+        // 첫 번째 조건: a가 b보다 작으면 1, 아니면 0을 출력
+        if (a < b) {
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
 
+        // 공백 출력
+        System.out.print(" ");
+
+        // 두 번째 조건: a가 b와 같으면 1, 아니면 0을 출력
+        if (a == b) {
+            System.out.print("1");
+        } else {
+            System.out.print("0");
+        }
+
+        // Scanner 닫기
+        sc.close();
     }
 }
