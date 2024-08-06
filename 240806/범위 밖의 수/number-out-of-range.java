f1318b5f069eb0d46430da3f9fe8b15a61598abd
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-08-06 14:14:43 +0900

    Title: 범위 밖의 수, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240806/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner 객체를 생성하여 입력을 받습니다.
+        Scanner scanner = new Scanner(System.in);
+
+        // 첫 번째 줄에서 a 값을 입력받습니다.
+        int a = scanner.nextInt();
+
+        // Scanner 객체를 닫습니다.
+        scanner.close();
+
+        // 조건을 확인하고 출력합니다.
+        if (a < 10 || a > 20) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
