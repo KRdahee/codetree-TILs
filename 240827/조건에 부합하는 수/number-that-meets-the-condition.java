9951ade2e04b2341218881dc027fbc2c9d285603
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-08-27 21:37:41 +0900

    Title: 조건에 부합하는 수, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240827/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 240827/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -11,15 +11,20 @@ public class Main {
         List<Integer> results = new ArrayList<>();
         
         for (int i = 1; i <= a; i++) {
-            boolean condition1 = (i % 2 == 0) && (i % 4 != 0);  // 짝수이면서 4의 배수가 아닌 수
-            boolean condition2 = (i / 8) % 2 == 0;  // 8로 나눈 몫이 짝수인 수
-            boolean condition3 = (i % 7) < 4;  // 7로 나눈 나머지가 4보다 작은 수
+            // 조건 1: 짝수이면서 4의 배수가 아닌 수
+            boolean condition1 = (i % 2 == 0) && (i % 4 != 0);
+            // 조건 2: 8로 나눈 몫이 짝수인 수
+            boolean condition2 = (i / 8) % 2 == 0;
+            // 조건 3: 7로 나눈 나머지가 4보다 작은 수
+            boolean condition3 = (i % 7) < 4;
             
+            // 모든 조건을 만족하지 않는 경우
             if (!(condition1 && condition2 && condition3)) {
                 results.add(i);
             }
         }
         
+        // 결과를 공백으로 구분하여 출력
         for (int num : results) {
             System.out.print(num + " ");
         }
