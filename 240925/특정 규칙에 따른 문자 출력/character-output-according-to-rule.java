fbb558217ed84b20d947542f4da22570e9eeaa78
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-09-25 22:01:16 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240925/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 240925/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -36,5 +36,31 @@ public class Main {
             System.out.print(" ");
         }
         scanner.close();
+    }
+}*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // 모양에 맞게 윗쪽 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++)
+                System.out.print("  ");
+            for(int j = 0; j <= i; j++)
+                System.out.print("@ ");
+            System.out.println();
+        }
+        
+        // 모양에 맞게 아랫쪽 별을 출력합니다.
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++)
+                System.out.print("@ ");
+            System.out.println();
+        }
     }
 }
