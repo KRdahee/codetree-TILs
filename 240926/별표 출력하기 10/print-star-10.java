2472964b048870c4053cbe3d2aeb822aaf48409c
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-09-26 11:37:21 +0900

    Title: 별표 출력하기 10, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240926/별표 출력하기 10/print-star-10.java
+++ 240926/별표 출력하기 10/print-star-10.java
@@ -1,37 +1,27 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 5;  // 테스트 케이스로 5를 사용합니다.
+
+        int n = scanner.nextInt();  // 사용자로부터 n 입력받기
+
+        // 별의 개수 배열
+        int[] starCounts = {1, 5, 2, 4, 3};
 
         // 상단 부분 출력
-        for (int i = 0; i < n; i++) {
-            int starCount;
-            if (i % 2 == 0) { // i가 짝수인 경우
-                starCount = 1 + (i / 2);
-            } else { // i가 홀수인 경우
-                starCount = n - (i - 1) / 2;
-            }
-
-            // 별 출력
-            for (int j = 0; j < starCount; j++) {
+        for (int count : starCounts) {
+            for (int j = 0; j < count; j++) {
                 System.out.print("* ");
             }
             System.out.println(); // 줄바꿈
         }
 
-        // 하단 부분 출력 (상단과 대칭)
-        for (int i = n - 1; i >= 0; i--) {
-            int starCount;
-            if (i % 2 == 0) { // i가 짝수인 경우
-                starCount = 1 + (i / 2);
-            } else { // i가 홀수인 경우
-                starCount = n - (i - 1) / 2;
-            }
+        // 하단 부분 출력 (대칭)
+        int[] lowerStarCounts = {3, 4, 2, 5, 1};
 
-            // 별 출력
-            for (int j = 0; j < starCount; j++) {
+        for (int count : lowerStarCounts) {
+            for (int j = 0; j < count; j++) {
                 System.out.print("* ");
             }
             System.out.println(); // 줄바꿈
