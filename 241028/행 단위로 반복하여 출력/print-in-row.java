66d1b2305af8285416fcff0d5b0f73f05dff8513
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-10-28 10:46:29 +0900

    Title: 행 단위로 반복하여 출력, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241028/행 단위로 반복하여 출력/print-in-row.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // n 입력 받기
+        int n = scanner.nextInt();
+        
+        // n의 값에 따른 숫자 출력
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j);
+            }
+            System.out.println(); // 다음 줄로 이동
+        }
+        
+        scanner.close();
+    }
+}
