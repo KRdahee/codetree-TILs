98d742ccc763b00c487d2f3360b96a454fb5f20e
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-10-31 14:48:20 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241031/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
+++ 241031/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -12,8 +12,8 @@ public class Main {
 
         // n x n 배열 생성 및 출력
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // 각 위치에 맞는 짝수 출력
+            for (int j = 0; j < n * 2; j++) {
+                // j가 n보다 클 경우, 반복하여 숫자 출력
                 System.out.print(evens[(i + j) % 4] + " ");
             }
             System.out.println(); // 다음 줄로 이동
