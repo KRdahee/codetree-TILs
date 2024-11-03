ab45fa3f0c6cf6c93e9c624b7a7c4a320b67c64c
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-11-03 22:21:14 +0900

    Title: 19단 출력, Time: 120ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241103/19단 출력/nineteen-times-table.java
+++ 241103/19단 출력/nineteen-times-table.java
@@ -4,11 +4,11 @@ public class Main {
         for (int i = 1; i <= 19; i++) {
             for (int j = 1; j <= 19; j++) {
                 // 각 곱셈 결과를 출력
-                System.out.println(i + " * " + j + " = " + (i * j));
+                System.out.print(i + " * " + j + " = " + (i * j));
                 
                 // j가 19가 아닌 경우 '/'를 추가하여 구분
                 if (j % 2 == 0 && j < 19) {
-                    System.out.print(" / ");
+                    System.out.println(" / ");
                 }
             }
             System.out.println(); // 각 i에 대해 줄바꿈
