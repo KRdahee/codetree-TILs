a0a9400354161999254905c6a69544aa47253a45
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-11-05 09:39:26 +0900

    Title: 약수가 세 개인 수, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241105/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 241105/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -2,25 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Scanner 객체를 통해 입력받기
         Scanner sc = new Scanner(System.in);
         
-        // start와 end 값 입력받기
+        // start와 end 값을 입력받음
         int start = sc.nextInt();
         int end = sc.nextInt();
         
-        int count = 0;
+        // 카운트 변수 초기화
+        int cnt = 0;
         
-        // start에서 end까지의 범위 내에서 완전 제곱수를 찾아 카운트
-        for (int i = 1; i * i <= end; i++) {
-            int square = i * i;
-            if (square >= start && square <= end) {
-                count++;  // 약수가 정확히 3개인 수 (완전 제곱수) 발견
+        // start부터 end까지 반복
+        for (int i = start; i <= end; i++) {
+            int sum = 0; // 약수의 개수를 셈
+            
+            // i의 약수를 찾는 부분
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    sum++; // 나누어 떨어지면 sum을 증가시킴
+                }
+            }
+            
+            // sum이 3이면 약수가 정확히 3개라는 의미
+            if (sum == 3) {
+                cnt++; // 조건에 맞으면 cnt를 증가
             }
         }
         
         // 결과 출력
-        System.out.println(count);
+        System.out.printf("%d", cnt);
         
+        // Scanner 객체 닫기
         sc.close();
     }
 }
